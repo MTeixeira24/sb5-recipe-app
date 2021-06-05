f17f3aaa326024bf08179e7c63264776cc2836bf
MTeixeira24 <miguelangelo_teixeira@hotmail.com>
2021-06-05 12:01:08 +0100

    Adding model

---
+++ src/main/java/pt/mteixeira/sb5recipeapp/domain/Notes.java
@@ -0,0 +1,45 @@
+package pt.mteixeira.sb5recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private Recipe recipes;
+
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Recipe recipes) {
+        this.recipes = recipes;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
