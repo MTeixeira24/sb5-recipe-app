0e5542c171627647bd9e4c65a9f12fc8a04f0f19
MTeixeira24 <miguelangelo_teixeira@hotmail.com>
2021-06-05 11:50:18 +0100

    Initial commit

---
+++ src/main/java/pt/mteixeira/sb5recipeapp/Sb5RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package pt.mteixeira.sb5recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb5RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb5RecipeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pt/mteixeira/sb5recipeapp/Sb5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.mteixeira.sb5recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb5RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
