48e7162db75cbe9aa848f3af1a4653041e731b37
MTeixeira24 <miguelangelo_teixeira@hotmail.com>
2021-09-02 14:23:07 +0100

    Add command object pojos

--- src/main/java/pt/mteixeira/sb5recipeapp/commands/UnitOfMeasureCommand.java
+++ src/main/java/pt/mteixeira/sb5recipeapp/commands/UnitOfMeasureCommand.java
@@ -1,4 +1,10 @@
 package pt.mteixeira.sb5recipeapp.commands;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class UnitOfMeasureCommand {
+    private String unit;
 }
--- src/main/java/pt/mteixeira/sb5recipeapp/domain/UnitOfMeasure.java
+++ src/main/java/pt/mteixeira/sb5recipeapp/domain/UnitOfMeasure.java
@@ -1,11 +1,17 @@
 package pt.mteixeira.sb5recipeapp.domain;
 
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 
 @Entity
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class UnitOfMeasure extends BaseEntity {
 
     private String unit;
